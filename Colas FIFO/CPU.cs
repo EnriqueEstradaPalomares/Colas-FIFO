3a3f6c131abff10581f903fccb2b325958a47fe4
EnriqueEstradaPalomares <36722043+EnriqueEstradaPalomares@users.noreply.github.com>
2018-05-27 12:41:48 -0500

    incompleto

---
+++ Colas FIFO/CPU.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas_FIFO
+{
+    class CPU
+    {
+        private Random rdn;
+        Proceso inicio = null, newProceso;
+        private int colaVacia = 0, proAtendidos = 0, proPendientes = 0, TTotal=0;
+        //-------------------------------------------------------------------
+        public void agregar(Proceso nuevo)
+        {
+            if (inicio == null)
+                inicio = nuevo;
+            else
+                agregar(nuevo, inicio);
+        }
+        private void agregar(Proceso nuevo, Proceso quien)
+        {
+            if (quien.sig != null)
+                agregar(nuevo, quien.sig);
+            else
+            {
+                quien.sig = nuevo;
+                TTotal += nuevo.Tiempo;
+            }
+        }
+
+        
+        //---------------------------------------------------------------------
+        public void start()
+        {
+            addProcess();
+            for(int i = 0; i < 300; i++)
+            {
+                if (probabilidad())
+                {
+                    addProcess();
+                }
+
+                if (inicio.Tiempo != 0)
+                {
+                    inicio.Tiempo -= 1;
+                }
+                else
+                {
+                    proAtendidos++;
+                    ifIsNullTime();
+                }
+                pendientes();
+            }
+        }
+        //---------------------------------------------------------------------
+        private void ifIsNullTime()
+        {
+            if (inicio.sig != null)
+            {
+                inicio = inicio.sig;
+            }
+            else
+                colaVacia++;
+        }
+        //---------------------------------------------------------------------
+        private void pendientes()
+        {
+            proPendientes = TTotal - proAtendidos;
+        }
+        //---------------------------------------------------------------------
+        public bool probabilidad()
+        {
+            int proba = 0;
+            rdn = new Random();
+            proba = rdn.Next(1, 101);
+            if (proba < 35)
+                return true;
+            else
+                return false;
+        }
+        //---------------------------------------------------------------------
+        public void addProcess()
+        {
+            rdn = new Random();
+            int time = rdn.Next(4, 15);
+            newProceso = new Proceso(time);
+            agregar(newProceso);
+            TTotal ++;
+        }
+        //-----------------------------------RETORNAR VALORES---------------------
+        public string retornaTotales()
+        {
+            string total = TTotal.ToString();
+            return total;
+        }
+        public string retornaVacias()
+        {
+            string vacias = colaVacia.ToString();
+            return vacias;
+        }
+        public string retornaAtendidos()
+        {
+            string atendidos = proAtendidos.ToString();
+            return atendidos;
+        }
+        public string retornaPendientes()
+        {
+            string pendientes = proPendientes.ToString();
+            return pendientes;
+        }
+        //--------------------------------------------------------------------------
+    }
+}
---
+++ Colas FIFO/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colas_FIFO
+{
+    public partial class Form1 : Form
+    {
+        //CPU cpu = new CPU();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            CPU cpu = new CPU();
+            cpu.start();
+            limpiar();
+            lblAtendidos.Text = cpu.retornaAtendidos();
+            lblPendientes.Text = cpu.retornaPendientes();
+            lblProcesos.Text = cpu.retornaTotales();
+            lblCiclos.Text = cpu.retornaVacias();
+            for(int i=0; i<300; i++)
+            {
+                if (timer())
+                    textBox1.Text += "|";
+                else
+                    textBox1.Text += "-";
+            }
+        }
+
+        private bool timer()
+        {
+            Random ono = new Random();
+            int ol = ono.Next(0, 2);
+            if (ol == 0)
+                return false;
+            else
+                return true;
+        }
+
+        private void limpiar()
+        {
+            textBox1.Clear();
+            lblAtendidos.Text = "";
+            lblCiclos.Text = "";
+            lblPendientes.Text = "";
+            lblProcesos.Text = "";
+        }
+    }
+}
